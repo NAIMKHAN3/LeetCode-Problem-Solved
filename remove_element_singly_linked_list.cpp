be5a9ddbd20a276140afff2abf4e29e3b261fde0
Naim Khan <naimkhan01856@gmail.com>
2024-07-04 10:40:38 +0600

    remove element done

---
+++ remove_element_singly_linked_list.cpp
@@ -0,0 +1,47 @@
+
+class ListNode
+{
+public:
+    int val;
+    ListNode *next;
+};
+
+class Solution
+{
+public:
+    ListNode *removeElements(ListNode *&head, int val)
+    {
+        if (head == NULL)
+            return head;
+        while (head != NULL && head->val == val)
+        {
+            if (head->next == NULL)
+            {
+                ListNode *deleteNode = head;
+                head = NULL;
+                delete deleteNode;
+                return head;
+            }
+            ListNode *deleteNode = head;
+            head = head->next;
+            delete deleteNode;
+        }
+
+        ListNode *temp = head;
+        while (temp != NULL && temp->next != NULL)
+        {
+            if (temp->next->val == val)
+            {
+                ListNode *deleteNode = temp->next;
+                temp->next = temp->next->next;
+                delete deleteNode;
+            }
+            if (temp->next != NULL && temp->next->val != val)
+            {
+
+                temp = temp->next;
+            }
+        }
+        return head;
+    }
+};
