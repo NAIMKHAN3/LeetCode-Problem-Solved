36b71dfc043c4f46fc9d115848206f053ea0dbfd
Naim Khan <naimkhan01856@gmail.com>
2024-07-04 14:11:26 +0600

    two sum done

---
+++ twoSum_vector.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> newV;
+      for(int i = 0;i<nums.size();i++){
+        for(int j = i+1; j<nums.size(); j++){
+            if((nums[i]+ nums[j]) == target){
+                newV.push_back(i);
+                newV.push_back(j);
+                break;
+            }
+        }
+      }
+      return newV;
+    }
+};
