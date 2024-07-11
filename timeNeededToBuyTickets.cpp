0b9eeca92a0e71e1dd1234675871edf3664df30b
Naim Khan <naimkhan01856@gmail.com>
2024-07-11 12:20:09 +0600

    buy ticket solved defferent way

--- timeNeededToBuyTickets.cpp
+++ timeNeededToBuyTickets.cpp
@@ -16,11 +16,9 @@ public:
         while (true)
         {
 
-           
-
             if (q.front() > 0)
             {
-                 s++;
+                s++;
                 if (inx.front() == k && q.front() == 1)
                     break;
                 q.push(q.front() - 1);
@@ -40,4 +38,35 @@ public:
         }
         return s;
     }
+};
+
+class Solution
+{
+public:
+    int timeRequiredToBuy(vector<int> &tickets, int k)
+    {
+        queue<int> q;
+        int s = 0;
+        for (int i = 0; i < tickets.size(); i++)
+        {
+            q.push(tickets[i]);
+        }
+        while (true)
+        {
+            q.front()--;
+            s++;
+            if (k == 0 && q.front() == 0)
+                break;
+            if (k == 0)
+                k = q.size() - 1;
+            else
+                k--;
+            if (q.front() != 0)
+                q.push(q.front());
+                q.pop();
+        }
+        return s;
+        
+    }
+    
 };
