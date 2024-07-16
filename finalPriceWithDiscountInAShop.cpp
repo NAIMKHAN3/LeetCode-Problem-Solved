54eac425db61bc17e11c03fb30ce746b058a9139
Naim Khan <naimkhan01856@gmail.com>
2024-07-16 14:44:30 +0600

    finalPrice problem solved

---
+++ finalPriceWithDiscountInAShop.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> finalPrices(vector<int> &prices)
+    {
+        for (int i = 0; i < prices.size(); i++)
+        {
+            int j = i+1;
+            while (j < prices.size()){
+                if(prices[j]<= prices[i]){
+                    prices[i] = prices[i] - prices[j];
+                    break;
+                }
+                j++;
+            }
+        }
+        return prices;
+    }
+};
